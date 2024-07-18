95b5d4bba014f7a70b2ed3e108cd1ff85930b949
Olga Bessonova <olga.sleepless@gmail.com>
2024-07-18 12:24:58 -0400

    Update styles

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React from 'react';
 import { useState } from 'react';
 import Stock from './components/stock/Stock';
-// import ChatGPT from './components/ChatGPT/oldChatGPT';
 import { ChatGPT } from './components/ChatGPT/ChatGPT';
 import { fetchStockData } from './components/stock/StockFetch';
 import { fetchChatGPT } from './components/ChatGPT/ChatGPTFetch';
@@ -10,6 +9,8 @@ import Chart from './components/Chart/Chart';
 import InvestorInfo from './components/InvestorInfo/InvestorInfo';
 import InvestingHorizon from './components/InvestingHorizon/InvestingHorizon';
 import stockTickers from './assets/company_tickers.json';
+import alexPhoto from './assets/photo/Alex.jpeg';
+import olgaPhoto from './assets/photo/Olga.jpeg';
 
 function App() {
     const [stockOne, setStockOne] = useState('');
@@ -68,23 +69,7 @@ function App() {
                     <h1 style={{ fontSize: '3em' }}>
                         compare stocks and find your best investment
                     </h1>
-                    <div className="created-by">
-                        <h2>created by alex and olga.</h2>
-                        <div
-                            style={{
-                                display: 'flex',
-                                gap: '20px',
-                                justifyContent: 'center',
-                            }}
-                        >
-                            <a href="https://github.com/luckyleck">
-                                Alex&apos;s Github
-                            </a>
-                            <a href="https://github.com/olga-bessonova">
-                                Olga&apos;s Github
-                            </a>
-                        </div>
-                    </div>
+
                 </div>
                 <div className="selection">
                     <div className="stock-inputs">
@@ -131,6 +116,26 @@ function App() {
                     stockOneData={stockOneData}
                     stockTwoData={stockTwoData}
                 />
+
+                <footer>
+                    <div className="created-by">
+                        <h2>Created by Alex Lecky and Olga Bessonova</h2>
+                        <div className="creators">
+                            <div className="creator">
+                                <a href="https://github.com/luckyleck">
+                                    <img src={alexPhoto} alt="Alex Lecky" />
+                                    <p>Alex Lecky</p>                                    
+                                </a>
+                            </div>                            
+                            <div className="creator">
+                                <a href="https://github.com/olga-bessonova">
+                                    <img src={olgaPhoto} alt="Olga Bessonova" />
+                                    <p>Olga Bessonova</p>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
             </div>
         </div>
     );
